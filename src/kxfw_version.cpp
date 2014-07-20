2d7aec130d4b5a870b82f34de95ef8f119a361dd
getwingm <getwingm@gmail.com>
2014-07-20 13:10:27 +0800

    还是用回vs2008版

--- src/kxfw_version.cpp
+++ src/kxfw_version.cpp
@@ -1,7 +1,12 @@
+#pragma once
+/*--------------------------------------------------------------------
+* don't changed this file,it wile autogernate.
+* the detail time is: 2014/07/20 ÷‹»’ 10:58:05.54
+--------------------------------------------------------------------*/
 #include "stdafx.h" 
 #include "kxfw_version.h"
 /*version string date*/  
 char *getKxfwBuildingTime()
 {
-    return "";
+    return "2014-07-20";
 }
